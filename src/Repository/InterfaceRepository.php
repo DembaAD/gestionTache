90512c01eb92bf6890fc43e48f1b9e884988cca2
DembaAD <dembadieng73@gmail.com>
2025-09-12 21:52:15 +0000

    Interface pour les Repository

---
+++ src/Repository/InterfaceRepository.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Repository;
+
+use App\Model\Task;
+
+interface InterfaceRepository{
+    public function findById($id);
+    public function create(Task $t);
+    public function delete($id);
+    public function update($id, Task $t);
+    public function all();
+}
