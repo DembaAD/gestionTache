a8d74cd08524096c56d32b630cec06327bb61877
DembaAD <dembadieng73@gmail.com>
2025-10-07 22:23:25 +0000

    Creation d'un routeur pour l'application

---
+++ src/Router.php
@@ -0,0 +1,27 @@
+<?php
+namespace App;
+
+class Router{
+    private array $routes = [];
+    
+    public function addRoute(Route $route){
+        $this->routes[] = $route;
+    }
+    public function dispatch(string $method, string $uri) {
+    foreach ($this->routes as $route) {
+        $params = $route->matches($method, $uri);
+        if ($params !== false) {
+            $handler = $route->getHandler();
+            if (is_callable($handler)) {
+                $handler($params); 
+            } else {
+                echo $handler;
+            }
+            return;
+        }
+    }
+    http_response_code(404);
+    echo "Page inexistante";
+}
+
+}
